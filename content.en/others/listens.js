1fc3291c96e533e6d737ab114c4303440a9a5cd5
arshsiddiqui <arshsiddiqui@gmail.com>
2023-12-12 16:10:32 -0500

    medium term listens

--- content.en/others/listens.js
+++ content.en/others/listens.js
@@ -32,6 +32,28 @@ async function getData() {
     });
 }
 
+async function getMedData() {
+    const response = await fetch('https://raw.githubusercontent.com/ArshSiddiqui/arshsiddiqui.github.io/main/50_med_term_songs.json');
+    const data = await response.json();
+    let songlist = document.getElementById('med-songs');
+    let lev = 1;
+    data.forEach(song => {
+        let elem = document.createElement('p');
+        elem.innerHTML = lev.toString()+". <a href='"+song['song_link']+"'>"+song['name']+"</a>";
+        let singer_elem = document.createElement('span');
+        singer_elem.innerHTML = "<a href='"+song['artist_url']+"'>"+song['artist']+"</a>";
+        singer_elem.style = "float: right;";
+        elem.appendChild(singer_elem);
+        songlist.appendChild(elem);
+        let preview_elem = document.createElement('p');
+        preview_elem.innerHTML = "<audio controls src='"+song['preview_link']+"' style='width:100%;height=5vh;margin-top:0px;'>";
+        songlist.appendChild(preview_elem);
+        // songlist.appendChild(singer_elem);
+        lev = lev + 1;
+    });
+}
+
 getData();
+getMedData();
 // var Spotify = require("spotify-web-api.js");
 // var s = new Spotify();
--- content.en/others/my_listens.md
+++ content.en/others/my_listens.md
@@ -5,9 +5,22 @@ type: docs
 
 # My Listens
 
-These are the songs I have been listening to the most in the past four weeks.  
+&nbsp;  
 
+
+## Short Term Listens
+These are the songs I have been listening to the most in the past four weeks.  
+___
 <div class="song-list">
     <p id="songs"></p>
 </div>
 <script src="../listens.js"></script>
+
+&nbsp;  
+
+## Medium Term Listens
+These are the songs I have been listening to the most in about the past half year.
+___
+<div class="med-song-list">
+    <p id="med-songs"></p>
+</div>
